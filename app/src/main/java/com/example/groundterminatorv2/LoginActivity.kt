2a2bc49d1007984dd8567c89e97db7b1978516d5
Simo <112989400+simoskoko@users.noreply.github.com>
2022-12-21 15:53:29 +0100

    Hardcode Acc
    Harcoded account works

--- app/src/main/java/com/example/groundterminatorv2/LoginActivity.kt
+++ app/src/main/java/com/example/groundterminatorv2/LoginActivity.kt
@@ -5,34 +5,45 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
 import com.example.groundterminatorv2.CameraActivity
 import com.example.groundterminatorv2.R
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val logClicked = findViewById<Button>(R.id.btnlog)
-        val usernameEntered = (findViewById<AppCompatEditText>(R.id.usernameField)).text.toString()
-        val passEntered = (findViewById<AppCompatEditText>(R.id.passwordField)).text.toString()
-
-        logClicked.setOnClickListener(View.OnClickListener() {
-            fun onClick()
-            {
-                if(usernameEntered == "daddy" && passEntered == "123")
-                {
-                    val logIntentCam = Intent(this, CameraActivity::class.java)
-                    startActivity(logIntentCam)
-                    finish()
-                }
-                else
-                    Toast.makeText(this, "Credentials invalid.", Toast.LENGTH_SHORT).show()
-            }
-        })
-
+        val btnlog = findViewById<Button>(R.id.btnlog)
+        val usernameField = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val passField = findViewById<EditText>(R.id.editTextTextPassword)
+        btnlog.setOnClickListener {
+            Toast.makeText(this, "Button pressed.", Toast.LENGTH_SHORT).show()
+            println("ABCD ${passField.text.toString()}  ${usernameField.text.toString()}")
+            if (usernameField.text.toString() == "daddy" && passField.text.toString() == "123") {
+                Toast.makeText(this, "Stuff happened", Toast.LENGTH_SHORT).show()
+                val logIntentCam = Intent(this, CameraActivity::class.java)
+                startActivity(logIntentCam)
+                finish()
+            } else
+                Toast.makeText(this, "Credentials invalid.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+//    fun fjaLog(view: View) {
+//        Toast.makeText(this, "Button pressed.", Toast.LENGTH_SHORT).show()
+//        if(usernameEntered == "daddy" && passEntered == "123")
+//        {
+//            Toast.makeText(this, "Stuff happened", Toast.LENGTH_SHORT).show()
+//            val logIntentCam = Intent(this, CameraActivity::class.java)
+//            startActivity(logIntentCam)
+//            finish()
+//        }
+//        else
+//            Toast.makeText(this, "Credentials invalid.", Toast.LENGTH_SHORT).show()
+//    }
