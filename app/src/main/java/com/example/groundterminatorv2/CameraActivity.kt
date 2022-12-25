6f412f73d5fd98cd919299098b72a7fd9dafafea
unknown <lukalukovic111@gmail.com>
2022-12-25 14:09:23 +0100

    dzeja

--- app/src/main/java/com/example/groundterminatorv2/CameraActivity.kt
+++ app/src/main/java/com/example/groundterminatorv2/CameraActivity.kt
@@ -2,10 +2,18 @@ package com.example.groundterminatorv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CameraActivity : AppCompatActivity() {
+
+    private val btnGoBack = findViewById<Button>(R.id.btnGoBack)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        btnGoBack.setOnClickListener {
+            finish()
+        }
     }
 }
