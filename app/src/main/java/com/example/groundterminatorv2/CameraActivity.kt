f52d797168f2c5d450331c9b7a092c3e2a85e658
Simo <112989400+simoskoko@users.noreply.github.com>
2022-12-28 16:50:26 +0100

    pokusaj

--- app/src/main/java/com/example/groundterminatorv2/CameraActivity.kt
+++ app/src/main/java/com/example/groundterminatorv2/CameraActivity.kt
@@ -2,10 +2,40 @@ package com.example.groundterminatorv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        val url = URL("http://localhost:5000")
+
+        with(url.openConnection() as HttpURLConnection)
+        {
+            requestMethod = "GET"
+            println("\n Sent 'GET' request to URL : $url; Response Code : $responseCode")
+
+//            inputStream.bufferedReader().use {
+//                it.lines().forEach { line ->
+//                    println(line)
+//                }
+//            }
+        }
+
+
+
+//        val connection = url.openConnection()
+//        BufferedReader(InputStreamReader(connection.getInputStream())).use { inp ->
+//            var line: String?
+//            while (inp.readLine().also { line = it } != null) {
+//                println(line)
+//            }
+//        }
     }
+
+
 }
--- app/src/main/java/com/example/groundterminatorv2/LoginActivity.kt
+++ app/src/main/java/com/example/groundterminatorv2/LoginActivity.kt
@@ -12,38 +12,48 @@ import androidx.appcompat.widget.AppCompatEditText
 import com.example.groundterminatorv2.CameraActivity
 import com.example.groundterminatorv2.R
 import com.google.android.material.textfield.TextInputEditText
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val client = HttpClient(CIO){
+            install(ContentNegotiation){
+
+            }
+        }
         val btnlog = findViewById<Button>(R.id.btnlog)
         val usernameField = findViewById<EditText>(R.id.editTextTextEmailAddress)
         val passField = findViewById<EditText>(R.id.editTextTextPassword)
         btnlog.setOnClickListener {
             Toast.makeText(this, "Button pressed.", Toast.LENGTH_SHORT).show()
-            println("ABCD ${passField.text.toString()}  ${usernameField.text.toString()}")
-            if (usernameField.text.toString() == "daddy" && passField.text.toString() == "123") {
-                Toast.makeText(this, "Stuff happened", Toast.LENGTH_SHORT).show()
-                val logIntentCam = Intent(this, CameraActivity::class.java)
-                startActivity(logIntentCam)
-                finish()
-            } else
-                Toast.makeText(this, "Credentials invalid.", Toast.LENGTH_SHORT).show()
+            println("ABCD ${passField.text}  ${usernameField.text}")
+
+//            val url = URL("http://localhost:5000")
+//            with(url.openConnection() as HttpURLConnection)
+//            {
+//                requestMethod = "GET"
+//                println("\n Sent 'GET' request to URL : $url; Response Code : $responseCode")
+//            if ( green light servera ) {
+//                val logIntentCam = Intent(this, CameraActivity::class.java)
+//                startActivity(logIntentCam)
+//                finish()
+//            } else
+//                Toast.makeText(this, "Credentials invalid.", Toast.LENGTH_SHORT).show()
+//            }
         }
     }
-}
 
-//    fun fjaLog(view: View) {
-//        Toast.makeText(this, "Button pressed.", Toast.LENGTH_SHORT).show()
-//        if(usernameEntered == "daddy" && passEntered == "123")
-//        {
-//            Toast.makeText(this, "Stuff happened", Toast.LENGTH_SHORT).show()
-//            val logIntentCam = Intent(this, CameraActivity::class.java)
-//            startActivity(logIntentCam)
-//            finish()
-//        }
-//        else
-//            Toast.makeText(this, "Credentials invalid.", Toast.LENGTH_SHORT).show()
-//    }
+    @Serializable
+    data class logInfo(val un: String, val pw: String)
+}
